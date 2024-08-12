bf2bf187fee48dcc751613030b245cefd3e238d0
kastro121 <castrokauasilva@gmail.com>
2024-08-12 20:48:26 -0300

    Add files via upload

---
+++ js/etapas.js
@@ -0,0 +1,56 @@
+let etapas = [
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero:'45',
+                nome:'EMANUEL LERYON',
+                partido:'Partido da Social Democratica Brasileira - PSDB',
+                fotos:[
+                    {url:'emanuel.jpg', legenda: 'Presidente'},
+                ]
+            },
+            {
+                numero:'22',
+                nome:'NOE KAYO',
+                partido:'Partido Liberal - PL',
+                fotos:[
+                    {url:'noe_kayo.jpg', legenda: 'Presidente'},
+                ]
+            },
+            {
+                numero:'44',
+                nome:'MIGUEL SIMÕES',
+                partido:'União Brasil - União',
+                fotos:[
+                    {url:'miguel_simoes.jpg', legenda: 'Presidente'},
+                ]
+            },
+            {
+                numero:'11',
+                nome:'HADED',
+                partido:'Partido Esportivo - PEsp',
+                fotos:[
+                    {url:'haded.jpg', legenda: 'Presidente'},
+                ]
+            },
+            {
+                numero:'13',
+                nome:'RAFAELLY',
+                partido:'Partido dos Trabalhadores - PT',
+                fotos:[
+                    {url:'rafaelly.jpg', legenda: 'Presidente'},
+                ]
+            },
+            {
+                numero:'50',
+                nome:'DANTE',
+                partido:'Partido Socialismo e Liberdade - PSOL',
+                fotos:[
+                    {url:'dante.jpg', legenda: 'Presidente'},
+                ]
+            },
+        ]
+    }
+];
